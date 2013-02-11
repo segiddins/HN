5146ed0c3c762f6ca157a3b5ca5495aefb2554e4
Samuel E. Giddins <segiddins@segiddins.me>
2013-02-10 22:00:36 -0500

    Mostly done! - custom browser + ipad working

---
+++ Hacker News/Hacker News/SEGRootViewController.h
@@ -0,0 +1,13 @@
+//
+//  SEGRootViewController.h
+//  Hacker News
+//
+//  Created by Samuel E. Giddins on 2/10/13.
+//  Copyright (c) 2013 Samuel E. Giddins. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SEGRootViewController : UISplitViewController
+
+@end
---
+++ Hacker News/Hacker News/SEGRootViewController.m
@@ -0,0 +1,38 @@
+//
+//  SEGRootViewController.m
+//  Hacker News
+//
+//  Created by Samuel E. Giddins on 2/10/13.
+//  Copyright (c) 2013 Samuel E. Giddins. All rights reserved.
+//
+
+#import "SEGRootViewController.h"
+
+@interface SEGRootViewController ()
+
+@end
+
+@implementation SEGRootViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
